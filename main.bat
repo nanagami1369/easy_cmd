af88335bb044d06cd183c8aaa1eec200d444606d
nasi nanagami <nanagami1369@gmail.com>
2018-09-04 23:43:15 +0900

    コマンドプロンプトで最初に読み込ませるファイルを作成

---
+++ main.bat
@@ -0,0 +1,12 @@
+@echo off
+REM cmd.exe‚É“Ç‚İ‚Ü‚¹‚éƒtƒ@ƒCƒ‹
+setlocal
+set green=[32;1m
+set default=[0m
+
+REM ˆ¥A•¶
+echo %green%Welcome to my cmd.exe%default%
+
+REM ƒGƒCƒŠƒAƒXƒtƒ@ƒCƒ‹
+doskey /macrofile=%HOME%/bat/doskey.txt
+endlocal
