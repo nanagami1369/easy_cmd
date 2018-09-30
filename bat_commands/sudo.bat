362e8c26ce4f68dbe3078e00a171eb35acd83dd2
nasi nanagami <nanagami1369@gmail.com>
2018-09-30 09:12:51 +0900

    プロファイルを読み込まないように変更

--- bat_commands/sudo.bat
+++ bat_commands/sudo.bat
@@ -1,20 +1,20 @@
 @echo off
 REM 管理者権限でコマンドプロンプトを開く
 if "%1"=="cmd" (
-	powershell  -Command start-process cmd -ArgumentList '/k main'-verb runas
+	powershell  -NoProfile start-process cmd -ArgumentList '/k main'-verb runas
 	exit /B
 )
 if "%1"=="su" (
-	powershell  -Command start-process cmd -ArgumentList '/k main'-verb runas
+	powershell  -NoProfile start-process cmd -ArgumentList '/k main'-verb runas
 	exit /B
 )
 if "%~x1"==".exe" (
-	powershell -Command Start-Process %1 -ArgumentList '"%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
+	powershell -NoProfile Start-Process %1 -ArgumentList '"%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
 	exit /B
 )
 REM コマンドはcmd.exeに流して実行
 if not "%1"=="" (
-	powershell -Command Start-Process cmd -ArgumentList '/K "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
+	powershell -NoProfile Start-Process cmd -ArgumentList '/K "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
 ) else (
 	echo コマンド構文が間違っています
 	echo sudo [実行するコマンド]
