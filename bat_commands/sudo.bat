10a72a2a96ecaf4400183300abc2cd7bcc4b19bf
nasi nanagami <nanagami1369@gmail.com>
2019-04-14 13:01:15 +0900

    コマンドでもeasyコマンドを読み込むように修正

--- bat_commands/sudo.bat
+++ bat_commands/sudo.bat
@@ -1,22 +1,23 @@
 @echo off
 REM 管理者権限でコマンドプロンプトを開く
 if "%1"=="cmd" (
-	powershell  -NoProfile start-process cmd -ArgumentList '/k easy "&" ""cd /d %CD%""'-verb runas
-	exit /B
+    powershell  -NoProfile start-process cmd -ArgumentList '/k easy "&" ""cd /d %CD%""'-verb runas
+    exit /B
 )
 if "%1"=="su" (
-	powershell  -NoProfile start-process cmd -ArgumentList '/k easy "&" ""cd /d %CD%""'-verb runas
-	exit /B
+    powershell  -NoProfile start-process cmd -ArgumentList '/k easy "&" ""cd /d %CD%""'-verb runas
+    exit /B
 )
 if "%~x1"==".exe" (
-	powershell -NoProfile Start-Process %1 -ArgumentList '"%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
-	exit /B
+    powershell -NoProfile Start-Process %1 -ArgumentList '"%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
+    exit /B
 )
 REM コマンドはcmd.exeに流して実行
 if not "%1"=="" (
-	powershell -NoProfile Start-Process cmd -ArgumentList '/K ""cd /d %CD%"" "&" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
+    powershell -NoProfile Start-Process cmd -ArgumentList '/K ""cd /d %CD%"" "&" "easy" "&" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
 ) else (
-	echo コマンド構文が間違っています
-	echo sudo [実行するコマンド]
-	exit /B
+    rem 引数がなければエラーを返す
+    echo コマンド構文が間違っています
+    echo sudo [実行するコマンド][実行するコマンドにつける引数]
+    exit /B
 )
