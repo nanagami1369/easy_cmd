bedd5235a30b35e7b765f6b68597df8c4874acff
nasi nanagami <nanagami1369@gmail.com>
2019-04-14 12:54:04 +0900

    最初に読みませるファイルを２つに分割

--- bat_commands/easy.bat
+++ bat_commands/easy.bat
@@ -7,6 +7,9 @@ set default=[0m
 REM ˆ¥A•¶
 echo %green%Welcome to my cmd.exe%default%
 
+REM İ’èƒtƒ@ƒCƒ‹(‚½‚¾‚Ìƒoƒbƒ`ƒtƒ@ƒCƒ‹)‚ğ“Ç‚İ‚Ş
+call setting_cmd.bat
+
 REM ƒGƒCƒŠƒAƒXƒtƒ@ƒCƒ‹
 doskey /macrofile=%HOME%/bat_commands/doskey.txt
 endlocal
