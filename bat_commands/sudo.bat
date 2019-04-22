c5c6a9b2ba1b7f74c3a2245967d83e33fbb00e77
nanagami1369 <nanagami1369@gmail.com>
2019-04-22 10:42:00 +0900

    powershellとpowersell Coreを短い引数で起動できるコマンドを作成

--- bat_commands/sudo.bat
+++ bat_commands/sudo.bat
@@ -8,6 +8,15 @@ if "%1"=="su" (
     powershell  -NoProfile start-process cmd -ArgumentList '/k easy "&" ""cd /d %CD%""'-verb runas
     exit /B
 )
+REM 管理者権限でPowerShellを起動するコマンド
+if "%1"=="posh" (
+    powershell  -NoProfile start-process powershell.exe -verb runas
+    exit /B
+)
+if "%1"=="pwsh" (
+    powershell  -NoProfile start-process pwsh.exe -verb runas
+    exit /B
+)
 REM [.exe]がついていた場合はアプリ自体を環境変数で起動するように変更
 if "%~x1"==".exe" (
     powershell -NoProfile Start-Process %1 -ArgumentList '"%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9" ' -Verb Runas
