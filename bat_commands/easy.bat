c654207606c8dfa78b28bdca581ccad356daa464
nanagami1369 <nanagami1369@gmail.com>
2022-03-12 18:15:55 +0900

    使われていないコードを削除

--- bat_commands/easy.bat
+++ bat_commands/easy.bat
@@ -2,12 +2,6 @@
 REM file is read to cmd.exe
 if not "%ReadScript%"=="true" (
 setlocal
-set green=[32;1m
-set default=[0m
-
-REM hello world
-REM echo %green%Welcome to my cmd.exe%default%
-
 REM read local setting file
 call setting_cmd.bat
 
