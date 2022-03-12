cfc3c792001e8f9ac71b6a9b927ff10a3687b587
nanagami1369 <nanagami1369@gmail.com>
2022-03-12 18:15:14 +0900

    設定ファイルを何度も読み込まないようにした

--- bat_commands/easy.bat
+++ bat_commands/easy.bat
@@ -1,5 +1,6 @@
 @echo off
 REM file is read to cmd.exe
+if not "%ReadScript%"=="true" (
 setlocal
 set green=[32;1m
 set default=[0m
@@ -13,5 +14,8 @@ call setting_cmd.bat
 REM alias
 doskey /macrofile=%HOME%/bat_commands/doskey.txt
 endlocal
+)
+set ReadScript=true
+
 REM prompt
 PROMPT $E[32;1m$P$E[0m$G
